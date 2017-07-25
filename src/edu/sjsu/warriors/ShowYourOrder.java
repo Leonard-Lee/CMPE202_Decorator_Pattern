81aafc4ddbf748d47dd2a440bee15806bd8bb735
Leonard Lee <archerleonard@gmail.com>
2017-07-25 14:58:03 -0700

    add a new one customized burger for meeting Prof's new requirements

--- src/edu/sjsu/warriors/ShowYourOrder.java
+++ src/edu/sjsu/warriors/ShowYourOrder.java
@@ -21,6 +21,7 @@ public class ShowYourOrder {
         System.out.println("Price: " + oldSchoolBurger.cost());
 
         // Your Own Burger
+        System.out.println("The first customized burger");
         Burger yourOwnBurger = new YourOwnBurger();
         // Step 1. Choose a Burger
         // meat name: Beef, Chicken, Turkey, Veggie
@@ -34,15 +35,38 @@ public class ShowYourOrder {
            "Bermuda Red Onion", "Black Olives",
                 "Carrot Strings", "Coleslaw"
         });
-        // Step 4. Premium Toppings
+        // Premium Toppings
         yourOwnBurger = new PremiumTopping(yourOwnBurger, new String[]{
                 "Applewood Smoked Bacon"
         });
-        // Step 5. Choose a Sauce
+        // Step 4. Choose a Sauce
         yourOwnBurger = new Sauce(yourOwnBurger, new String[]{
                 "Apricot Sauce"
         });
         System.out.println("Order: " + yourOwnBurger.getDescription());
         System.out.println("Price: " + yourOwnBurger.cost());
+
+        // The second customized burger
+        System.out.println("The second customized burger");
+        Burger yourOwnBurger2 = new YourOwnBurger();
+        // Step 1. Choose a Burger
+        // meat name: Beef, Chicken, Turkey, Veggie
+        // lb: 1/3lb, 2/3lb, 1lb
+        yourOwnBurger2 = new Meat(yourOwnBurger2, "Beef", "1/3lb");
+        yourOwnBurger2 = new Bowl(yourOwnBurger2, "Greens");
+        // Step 2. Choose a Cheese
+        yourOwnBurger2 = new Cheese(yourOwnBurger2, new String[]{"Greek Feta", "Imported Swiss", "Sharp Provolone"});
+        // Step 3. Choose up to 4 Toppings
+        yourOwnBurger2 = new Topping(yourOwnBurger2, new String[]{
+                "Bermuda Red Onion", "Black Olives",
+                "Carrot Strings", "Coleslaw", "Dill Pickle Chips"
+        });
+
+        // Step 5. Choose a Sauce
+        yourOwnBurger2 = new Sauce(yourOwnBurger2, new String[]{
+                "Brazil Pesto", "Classic Caesar"
+        });
+        System.out.println("Order: " + yourOwnBurger2.getDescription());
+        System.out.println("Price: " + yourOwnBurger2.cost());
     }
 }
